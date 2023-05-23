2141bc3207ffee786ada2f1dd5e47483c0b60a7b
HManhTien <hoangtienvhdh@gmail.com>
2023-05-23 14:02:37 +0700

    ok , test

---
+++ Chatele/Form1.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Windows.Forms;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+using Telegram.Bot;
+using Message = Telegram.Bot.Types.Message;
+
+namespace Chatele
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TelegramBotClient Botclient;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Botclient = new TelegramBotClient("6118029010:AAGVF3-PnfU8nqoDDlApjCKVl-GuNCGyqEc");
+            Starreview();
+        }
+
+        public async Task Starreview()
+        {
+            var token = new CancellationTokenSource();
+            var canceltoken = token.Token;
+            var reOpt = new ReceiverOptions { AllowedUpdates = { } };
+            await Botclient.ReceiveAsync(OnMessage, ErrorMessage, reOpt, canceltoken);
+        }
+
+        public async Task OnMessage(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is Message message)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, " helobro ");
+            } 
+        }
+
+        public async Task ErrorMessage(ITelegramBotClient telegramBot, Exception e, CancellationToken cancellationToken)
+        {
+            if (e is ApiRequestException requestException)
+            {
+                await Botclient.SendTextMessageAsync(" ", e.Message.ToString());
+            }
+        }
+    }
+}
---
+++ connnectC#_SQL/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+
+namespace connnectC__SQL
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        String str = @"Data Source=HOANGTIEN\\SQL;Initial Catalog=BANHANG;Intted Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        void loaddata()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select* from Khach_Hang";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Data Source=HOANGTIEN\\SQL;Initial Catalog=BANHANG;Integrated Security=True;";
+            sqlConnection.Open();
+            loaddata();
+
+        }
+    }
+}
